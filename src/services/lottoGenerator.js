ff03f15cb292c498ffda8107b38d7b4923935166
ryeong <oaoa0728@naver.com>
2025-11-01 17:48:20 +0900

    feat(generator): 로또 자동 발급 구현

---
+++ src/services/lottoGenerator.js
@@ -0,0 +1,14 @@
+import { Random } from '@woowacourse/mission-utils';
+import { LOTTO } from '../constants/rules.js';
+import Lotto from '../domain/Lotto.js';
+
+const pickNumbers = function pickNumbers(rand = Random) {
+  return rand.pickUniqueNumbersInRange(LOTTO.MIN, LOTTO.MAX, LOTTO.COUNT);
+};
+
+const issueLotto = (amount, rand = Random) => {
+  const count = amount / LOTTO.PRICE;
+  return Array.from({ length: count }, () => new Lotto(pickNumbers(rand)));
+};
+
+export { issueLotto };
